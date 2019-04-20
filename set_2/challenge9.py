799ef9e924ad9bbbee116c6bfc6201cd98bc0b6b
tildedennis <dennis.schwarz1@gmail.com>
2019-04-20 17:04:28 -0400

    rename some vars to get my thinking straight

--- set_2/challenge9.py
+++ set_2/challenge9.py
@@ -2,69 +2,74 @@ import struct
 import sys
 
 
-def pkcs7_pad(block_size, block):
-    if len(block) % block_size == 0:
-        return block
+def pkcs7_pad(block_size, buf):
+    if len(buf) % block_size == 0:
+        return buf
 
-    complete_blocks = len(block) / block_size
+    complete_blocks = len(buf) / block_size
     padded_size = (complete_blocks + 1) * block_size
-    num_padding_bytes = padded_size - len(block)
+    num_padding_bytes = padded_size - len(buf)
     pad_byte = struct.pack("B", num_padding_bytes)
 
-    padded_block = block + (pad_byte*num_padding_bytes)
-    if len(padded_block) % block_size != 0:
+    padded_buf = buf + (pad_byte*num_padding_bytes)
+    if len(padded_buf) % block_size != 0:
         print "bad padding added"
-        return block
+        return buf
 
-    return padded_block
+    return padded_buf
 
 
-def pkcs7_unpad(block_size, padded_block):
-    if len(padded_block) != block_size:
+def pkcs7_unpad(block_size, padded_buf):
+    if len(padded_buf) % block_size != 0:
         print "bad padded length"
-        return padded_block
+        return padded_buf
 
-    possible_pad_byte = padded_block[-1]
-    if ord(possible_pad_byte) >= len(padded_block):
-        return padded_block
+    possible_pad_byte = padded_buf[-1]
+    if ord(possible_pad_byte) >= len(padded_buf):
+        return padded_buf
 
     pad_bytes = possible_pad_byte*ord(possible_pad_byte)
-    if padded_block[-ord(possible_pad_byte):] != pad_bytes:
-        return padded_block
+    if padded_buf[-ord(possible_pad_byte):] != pad_bytes:
+        return padded_buf
 
-    return padded_block[:-ord(possible_pad_byte)]
+    return padded_buf[:-ord(possible_pad_byte)]
 
 
 if __name__ == "__main__":
     block_size = 20
 
     print "padding"
-    block1 = "A"*block_size
-    padded_block1 = pkcs7_pad(block_size, block1)
-    print "%s (%d bytes)" % (padded_block1, len(padded_block1))
+    buf1 = "A"*block_size
+    padded_buf1 = pkcs7_pad(block_size, buf1)
+    print "%s (%d bytes)" % (padded_buf1, len(padded_buf1))
 
-    block2 = "YELLOW SUBMARINE"
-    padded_block2 = pkcs7_pad(block_size, "YELLOW SUBMARINE")
-    print "%s (%d bytes)" % (padded_block2, len(padded_block2))
-    if padded_block2 != "YELLOW SUBMARINE\x04\x04\x04\x04":
+    buf2 = "YELLOW SUBMARINE"
+    padded_buf2 = pkcs7_pad(block_size, "YELLOW SUBMARINE")
+    print "%s (%d bytes)" % (padded_buf2, len(padded_buf2))
+    if padded_buf2 != "YELLOW SUBMARINE\x04\x04\x04\x04":
         print "bad padding"
         sys.exit(1)
 
     print "unpadding"
-    unpadded_block1 = pkcs7_unpad(block_size, padded_block1)
-    print "%s (%d bytes)" % (unpadded_block1, len(unpadded_block1))
-    if unpadded_block1 != block1:
+    unpadded_buf1 = pkcs7_unpad(block_size, padded_buf1)
+    print "%s (%d bytes)" % (unpadded_buf1, len(unpadded_buf1))
+    if unpadded_buf1 != buf1:
         print "bad unpadding"
         sys.exit(1)
 
-    unpadded_block2 = pkcs7_unpad(block_size, padded_block2)
-    print "%s (%d bytes)" % (unpadded_block2, len(unpadded_block2))
-    if unpadded_block2 != block2:
+    unpadded_buf2 = pkcs7_unpad(block_size, padded_buf2)
+    print "%s (%d bytes)" % (unpadded_buf2, len(unpadded_buf2))
+    if unpadded_buf2 != buf2:
         print "bad unpadding"
         sys.exit(1)
 
-    block = "A"*34
-    padded_block = pkcs7_pad(16, block)
-    if padded_block != "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e":
+    buf3 = "A"*34
+    padded_buf3 = pkcs7_pad(16, buf3)
+    if padded_buf3 != "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e\x0e":
         print "bad padding"
+        sys.exit(1)
+
+    unpadded_buf3 = pkcs7_unpad(16, padded_buf3)
+    if unpadded_buf3 != buf3:
+        print "bad unpadding"
         sys.exit(1)
